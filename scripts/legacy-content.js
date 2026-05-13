405e3ec985dc5cfa1c28326332749e42a743a548
Issac Tabares <issacquantum@users.noreply.github.com>
2026-05-13 10:40:51 -0600

    Restore GT vehicle clips without technical videos

--- scripts/legacy-content.js
+++ scripts/legacy-content.js
@@ -1,5 +1,5 @@
 import { bigBangLegacyContent } from "../data/legacy-big-bang.js";
-import { getCachedDocument, getCachedDocumentNow, hasCachedDocument } from "./content-cache.js?v=20260513-gt-media-trim";
+import { getCachedDocument, getCachedDocumentNow, hasCachedDocument } from "./content-cache.js?v=20260513-gt-vehicles-restore";
 import { pick } from "./i18n.js";
 
 let activeRequestToken = 0;
