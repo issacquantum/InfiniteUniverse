77d455d7a5ad3b511a2bb6c92ec829939f8eab54
Issac Tabares <issacquantum@users.noreply.github.com>
2026-05-19 11:11:23 -0600

    Add phone science menu close button

--- scripts/app.js
+++ scripts/app.js
@@ -6,7 +6,7 @@ import { refreshIcons } from "./icons.js";
 import { pick } from "./i18n.js";
 import { syncLegacyContent } from "./legacy-content.js?v=20260513-gt-vehicles-restore";
 import { createMusicController, syncMusicUi } from "./music.js?v=20260513-tekken-delayed-autoplay-unlock";
-import { renderSite } from "./render.js?v=20260519-desktop-science-menu";
+import { renderSite } from "./render.js?v=20260519-phone-science-close";
 import { createState } from "./state.js";
 import { syncStructuredContent } from "./structured-content.js?v=20260514-orbital-purple-nucleus";
 
