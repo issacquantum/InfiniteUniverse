e702c939c5aaf962d4843d54d09446c6e52f0a48
Issac Tabares <issacquantum@users.noreply.github.com>
2026-05-20 22:36:29 -0600

    Reframe technical interests section

--- scripts/legacy-content.js
+++ scripts/legacy-content.js
@@ -1,5 +1,5 @@
 import { bigBangLegacyContent } from "../data/legacy-big-bang.js";
-import { getCachedDocument, getCachedDocumentNow, hasCachedDocument } from "./content-cache.js?v=20260520-glossary-statmech-foliation";
+import { getCachedDocument, getCachedDocumentNow, hasCachedDocument } from "./content-cache.js?v=20260520-other-science-interests";
 import { pick } from "./i18n.js";
 
 let activeRequestToken = 0;
