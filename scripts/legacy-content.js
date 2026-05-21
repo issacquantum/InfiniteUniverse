0092b165002a4610b77330190f373d2e964d7514
Issac Tabares <issacquantum@users.noreply.github.com>
2026-05-20 19:55:19 -0600

    Improve Spanish early life wording

--- scripts/legacy-content.js
+++ scripts/legacy-content.js
@@ -1,5 +1,5 @@
 import { bigBangLegacyContent } from "../data/legacy-big-bang.js";
-import { getCachedDocument, getCachedDocumentNow, hasCachedDocument } from "./content-cache.js?v=20260520-equation-pages";
+import { getCachedDocument, getCachedDocumentNow, hasCachedDocument } from "./content-cache.js?v=20260520-spanish-early-life-wording";
 import { pick } from "./i18n.js";
 
 let activeRequestToken = 0;
