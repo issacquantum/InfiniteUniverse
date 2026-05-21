d0e9b68ce0ef0745b45fc2011704934618eafb4d
Issac Tabares <issacquantum@users.noreply.github.com>
2026-05-20 18:07:19 -0600

    Improve equation explanation pages

--- scripts/legacy-content.js
+++ scripts/legacy-content.js
@@ -1,5 +1,5 @@
 import { bigBangLegacyContent } from "../data/legacy-big-bang.js";
-import { getCachedDocument, getCachedDocumentNow, hasCachedDocument } from "./content-cache.js?v=20260519-source-language-clean";
+import { getCachedDocument, getCachedDocumentNow, hasCachedDocument } from "./content-cache.js?v=20260520-equation-pages";
 import { pick } from "./i18n.js";
 
 let activeRequestToken = 0;
