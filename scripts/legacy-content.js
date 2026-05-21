4cb47066744154b114e29dbd01db67261a5212e6
Issac Tabares <issacquantum@users.noreply.github.com>
2026-05-20 21:55:06 -0600

    Remove Nurburgring brand list

--- scripts/legacy-content.js
+++ scripts/legacy-content.js
@@ -1,5 +1,5 @@
 import { bigBangLegacyContent } from "../data/legacy-big-bang.js";
-import { getCachedDocument, getCachedDocumentNow, hasCachedDocument } from "./content-cache.js?v=20260520-spanish-importa-wording";
+import { getCachedDocument, getCachedDocumentNow, hasCachedDocument } from "./content-cache.js?v=20260520-nurburgring-no-brand-list";
 import { pick } from "./i18n.js";
 
 let activeRequestToken = 0;
