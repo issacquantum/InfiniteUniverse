d2390bcca4bc3803bf774260188fc1d2a4e82a96
Issac Tabares <issacquantum@users.noreply.github.com>
2026-05-20 23:04:30 -0600

    Correct core questions wording

--- scripts/legacy-content.js
+++ scripts/legacy-content.js
@@ -1,5 +1,5 @@
 import { bigBangLegacyContent } from "../data/legacy-big-bang.js";
-import { getCachedDocument, getCachedDocumentNow, hasCachedDocument } from "./content-cache.js?v=20260520-other-science-interests";
+import { getCachedDocument, getCachedDocumentNow, hasCachedDocument } from "./content-cache.js?v=20260520-core-questions";
 import { pick } from "./i18n.js";
 
 let activeRequestToken = 0;
