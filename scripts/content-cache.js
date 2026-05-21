6dbc911ac9a4f746100e91431c598c3eacdd9cd3
Issac Tabares <issacquantum@users.noreply.github.com>
2026-05-20 21:15:27 -0600

    Start music section with piano memory

--- scripts/content-cache.js
+++ scripts/content-cache.js
@@ -3,7 +3,7 @@ import { siteContent } from "../data/site-content.js";
 
 const documentCache = new Map();
 const requestCache = new Map();
-const CONTENT_CACHE_VERSION = "20260520-site-version-notice";
+const CONTENT_CACHE_VERSION = "20260520-music-starts-piano";
 const PREWARM_WORKER_LIMIT = 4;
 let prewarmScheduled = false;
 
