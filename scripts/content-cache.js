2608a7b3b9f572395655f6b9dd0265fd85f72a28
Issac Tabares <issacquantum@users.noreply.github.com>
2026-05-20 20:41:17 -0600

    Shorten Spanish IPN references

--- scripts/content-cache.js
+++ scripts/content-cache.js
@@ -3,7 +3,7 @@ import { siteContent } from "../data/site-content.js";
 
 const documentCache = new Map();
 const requestCache = new Map();
-const CONTENT_CACHE_VERSION = "20260520-education-remove-bullying";
+const CONTENT_CACHE_VERSION = "20260520-spanish-ipn-short";
 const PREWARM_WORKER_LIMIT = 4;
 let prewarmScheduled = false;
 
