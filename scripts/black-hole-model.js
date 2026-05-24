f8d5982e8492a7717b854d938849b4b853c6258e
Issac Tabares <issacquantum@users.noreply.github.com>
2026-05-24 14:53:11 -0600

    Open science menu from active titles

--- scripts/black-hole-model.js
+++ scripts/black-hole-model.js
@@ -1,4 +1,4 @@
-import { bindPinchZoom } from "./model-pan.js?v=20260524-science-menu-reset-v1";
+import { bindPinchZoom } from "./model-pan.js?v=20260524-active-science-title-menu-v1";
 
 const mountedModels = new WeakSet();
 
