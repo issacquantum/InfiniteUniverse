38cee93e0a37e5e2b92bb6c7f5109780827b2877
Issac Tabares <issacquantum@users.noreply.github.com>
2026-05-24 13:59:54 -0600

    Add desktop reader arrows

--- scripts/orbital-selector-model.js
+++ scripts/orbital-selector-model.js
@@ -1,4 +1,4 @@
-import { bindPinchZoom, isModelPanGesture, panObjectFromPointer } from "./model-pan.js?v=20260524-authorized-text-colors-v1";
+import { bindPinchZoom, isModelPanGesture, panObjectFromPointer } from "./model-pan.js?v=20260524-desktop-reader-arrows-v1";
 
 const mountedModels = new WeakSet();
 let threePromise = null;
