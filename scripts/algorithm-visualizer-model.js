70cca6c0b9b1b595fc368d5fccf6ead5e4039e48
Issac Tabares <issacquantum@users.noreply.github.com>
2026-05-24 15:26:19 -0600

    Remove quantum circuit model

--- scripts/algorithm-visualizer-model.js
+++ scripts/algorithm-visualizer-model.js
@@ -1,4 +1,4 @@
-import { bindPinchZoom, isModelPanGesture, panTargetFromPointer } from "./model-pan.js?v=20260524-phone-home-hidden-v1";
+import { bindPinchZoom, isModelPanGesture, panTargetFromPointer } from "./model-pan.js?v=20260524-remove-quantum-circuit-model-v1";
 
 const mountedModels = new WeakSet();
 let threePromise = null;
