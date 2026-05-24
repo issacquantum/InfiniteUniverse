dc82f703c1f239dc998cf3ac5a134800548ae603
Issac Tabares <issacquantum@users.noreply.github.com>
2026-05-24 14:30:19 -0600

    Reset science menu from active sections

--- scripts/information-theory-model.js
+++ scripts/information-theory-model.js
@@ -1,4 +1,4 @@
-import { bindPinchZoom } from "./model-pan.js?v=20260524-revert-quantum-circuit-v1";
+import { bindPinchZoom } from "./model-pan.js?v=20260524-science-menu-reset-v1";
 
 const mountedModels = new WeakSet();
 const SYMBOL_COUNT = 4;
--- scripts/render.js
+++ scripts/render.js
@@ -1,4 +1,4 @@
-import { pick } from "./i18n.js?v=20260524-revert-quantum-circuit-v1";
+import { pick } from "./i18n.js?v=20260524-science-menu-reset-v1";
 
 function escapeHtml(value) {
   return String(value)
