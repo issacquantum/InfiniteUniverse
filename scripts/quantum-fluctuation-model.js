abbc00fd8436d45ac9181c4b96ba0b36d728578c
Issac Tabares <issacquantum@users.noreply.github.com>
2026-05-24 13:30:07 -0600

    Upgrade quantum circuit model visuals

--- scripts/quantum-fluctuation-model.js
+++ scripts/quantum-fluctuation-model.js
@@ -1,4 +1,4 @@
-import { bindPinchZoom, isModelPanGesture, panTargetFromPointer } from "./model-pan.js?v=20260524-equation-spacing-v1";
+import { bindPinchZoom, isModelPanGesture, panTargetFromPointer } from "./model-pan.js?v=20260524-quantum-circuit-3d-v1";
 
 const mountedModels = new WeakSet();
 let threePromise = null;
