46973553e9b511895db76372eba5b9c12a07fcf8
Issac Tabares <issacquantum@users.noreply.github.com>
2026-05-24 14:19:44 -0600

    Revert quantum circuit model visuals

--- data/site-content.js
+++ data/site-content.js
@@ -1,4 +1,4 @@
-import { bigBangLegacyContent } from "./legacy-big-bang.js?v=20260524-desktop-home-button-v1";
+import { bigBangLegacyContent } from "./legacy-big-bang.js?v=20260524-revert-quantum-circuit-v1";
 
 function htmlSource(group, id) {
   return {
