77b7b605895936822b6afb4517463194a8694b3d
Issac Tabares <issacquantum@users.noreply.github.com>
2026-05-24 15:12:58 -0600

    Restrict home button to desktop

--- scripts/black-hole-model.js
+++ scripts/black-hole-model.js
@@ -1,4 +1,4 @@
-import { bindPinchZoom } from "./model-pan.js?v=20260524-gravity-fabric-fit-v1";
+import { bindPinchZoom } from "./model-pan.js?v=20260524-desktop-home-only-v1";
 
 const mountedModels = new WeakSet();
 
