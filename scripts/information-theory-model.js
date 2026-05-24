5a2d5eae881de3be88e07e9e2d6bba4762781aa4
Issac Tabares <issacquantum@users.noreply.github.com>
2026-05-24 13:59:54 -0600

    Add desktop reader arrows

--- scripts/information-theory-model.js
+++ scripts/information-theory-model.js
@@ -1,4 +1,4 @@
-import { bindPinchZoom } from "./model-pan.js?v=20260524-authorized-text-colors-v1";
+import { bindPinchZoom } from "./model-pan.js?v=20260524-desktop-reader-arrows-v1";
 
 const mountedModels = new WeakSet();
 const SYMBOL_COUNT = 4;
