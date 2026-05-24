425c9eb702a6c937381949441728ae65ec2882c8
Issac Tabares <issacquantum@users.noreply.github.com>
2026-05-24 15:19:01 -0600

    Hide home button on phone layouts

--- data/site-content.js
+++ data/site-content.js
@@ -1,4 +1,4 @@
-import { bigBangLegacyContent } from "./legacy-big-bang.js?v=20260524-desktop-home-only-v1";
+import { bigBangLegacyContent } from "./legacy-big-bang.js?v=20260524-phone-home-hidden-v1";
 
 function htmlSource(group, id) {
   return {
--- scripts/information-theory-model.js
+++ scripts/information-theory-model.js
@@ -1,4 +1,4 @@
-import { bindPinchZoom } from "./model-pan.js?v=20260524-desktop-home-only-v1";
+import { bindPinchZoom } from "./model-pan.js?v=20260524-phone-home-hidden-v1";
 
 const mountedModels = new WeakSet();
 const SYMBOL_COUNT = 4;
