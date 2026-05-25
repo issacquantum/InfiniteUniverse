b9a689217c2685496a43ccbcd714e41ffa03e3cb
Issac Tabares <issacquantum@users.noreply.github.com>
2026-05-24 22:29:19 -0600

    Standardize equation reading pages

--- scripts/orbital-selector-model.js
+++ scripts/orbital-selector-model.js
@@ -1,4 +1,4 @@
-import { bindPinchZoom, isModelPanGesture, panObjectFromPointer } from "./model-pan.js?v=20260524-science-depth-pass-v1";
+import { bindPinchZoom, isModelPanGesture, panObjectFromPointer } from "./model-pan.js?v=20260524-equation-reading-structure-v1";
 
 const mountedModels = new WeakSet();
 let threePromise = null;
