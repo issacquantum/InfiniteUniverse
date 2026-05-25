57fd10e86f1a979425c444893047a8a87a58be6b
Issac Tabares <issacquantum@users.noreply.github.com>
2026-05-24 20:48:23 -0600

    Rewrite equation meaning context sections

--- scripts/black-hole-model.js
+++ scripts/black-hole-model.js
@@ -1,4 +1,4 @@
-import { bindPinchZoom } from "./model-pan.js?v=20260524-equation-symbol-lists-v1";
+import { bindPinchZoom } from "./model-pan.js?v=20260524-equation-context-meaning-v1";
 
 const mountedModels = new WeakSet();
 
