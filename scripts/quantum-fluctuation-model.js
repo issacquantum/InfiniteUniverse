58c2f9aab4760fd6cdffea6caf72576809f16c4d
Issac Tabares <issacquantum@users.noreply.github.com>
2026-05-29 18:28:30 -0600

    Fix Spanish mechanics wording

--- scripts/quantum-fluctuation-model.js
+++ scripts/quantum-fluctuation-model.js
@@ -1,4 +1,4 @@
-import { bindPinchZoom, isModelPanGesture, panTargetFromPointer } from "./model-pan.js?v=20260529-equation-meaning-audit-v1";
+import { bindPinchZoom, isModelPanGesture, panTargetFromPointer } from "./model-pan.js?v=20260529-mechanics-wording-v1";
 
 const mountedModels = new WeakSet();
 let threePromise = null;
