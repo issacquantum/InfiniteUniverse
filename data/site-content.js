827b95b46e126d229a4d0b122740398bedd254f4
Issac Tabares <issacquantum@users.noreply.github.com>
2026-05-29 18:34:54 -0600

    Clarify classical mechanics vector caption

--- data/site-content.js
+++ data/site-content.js
@@ -1,4 +1,4 @@
-import { bigBangLegacyContent } from "./legacy-big-bang.js?v=20260529-mechanics-wording-v1";
+import { bigBangLegacyContent } from "./legacy-big-bang.js?v=20260529-vector-caption-v1";
 
 function htmlSource(group, id) {
   return {
--- scripts/gravity-lensing-model.js
+++ scripts/gravity-lensing-model.js
@@ -1,4 +1,4 @@
-import { bindPinchZoom } from "./model-pan.js?v=20260529-mechanics-wording-v1";
+import { bindPinchZoom } from "./model-pan.js?v=20260529-vector-caption-v1";
 
 const mountedModels = new WeakSet();
 const TWO_PI = Math.PI * 2;
