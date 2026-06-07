21e6172db3381ca32e68113726c8461e9c96ae4c
Issac Tabares <issacquantum@users.noreply.github.com>
2026-06-07 17:29:39 -0600

    Add icons to navigation buttons

--- scripts/numerical-methods-model.js
+++ scripts/numerical-methods-model.js
@@ -1,4 +1,4 @@
-import { bindPinchZoom, isModelPanGesture, panTargetFromPointer } from "./model-pan.js?v=20260607-electromagnetic-wave-3d-v1";
+import { bindPinchZoom, isModelPanGesture, panTargetFromPointer } from "./model-pan.js?v=20260607-nav-icons-v1";
 
 const mountedModels = new WeakSet();
 let threePromise = null;
