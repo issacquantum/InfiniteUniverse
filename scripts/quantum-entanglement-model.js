f2a74ba08def9568859563d5ec5aa999a055552d
Issac Tabares <issacquantum@users.noreply.github.com>
2026-06-07 17:15:23 -0600

    Add home overview map

--- scripts/quantum-entanglement-model.js
+++ scripts/quantum-entanglement-model.js
@@ -1,4 +1,4 @@
-import { bindPinchZoom, isModelPanGesture, panObjectFromPointer } from "./model-pan.js?v=20260607-electromagnetic-wave-3d-v1";
+import { bindPinchZoom, isModelPanGesture, panObjectFromPointer } from "./model-pan.js?v=20260607-home-overview-map-v1";
 
 const mountedModels = new WeakSet();
 let threePromise = null;
