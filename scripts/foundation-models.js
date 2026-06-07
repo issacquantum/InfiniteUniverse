d9932c2790739980a81aa5e54ce23c621adbb775
Issac Tabares <issacquantum@users.noreply.github.com>
2026-06-07 17:29:39 -0600

    Add icons to navigation buttons

--- scripts/foundation-models.js
+++ scripts/foundation-models.js
@@ -1,4 +1,4 @@
-import { bindPinchZoom } from "./model-pan.js?v=20260607-electromagnetic-wave-3d-v1";
+import { bindPinchZoom } from "./model-pan.js?v=20260607-nav-icons-v1";
 
 const mountedModels = new WeakSet();
 let threePromise = null;
