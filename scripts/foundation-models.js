338f16f079be11578a6bb541bff44637182f0902
Issac Tabares <issacquantum@users.noreply.github.com>
2026-06-07 18:55:09 -0600

    Stabilize reading route rail

--- scripts/foundation-models.js
+++ scripts/foundation-models.js
@@ -1,4 +1,4 @@
-import { bindPinchZoom } from "./model-pan.js?v=20260607-mature-watermarks-v1";
+import { bindPinchZoom } from "./model-pan.js?v=20260607-static-reading-rail-v1";
 
 const mountedModels = new WeakSet();
 let threePromise = null;
