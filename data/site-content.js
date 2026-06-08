09a7cb22f838cd0d07f2cf82b7a31f4951b5e10d
Issac Tabares <issacquantum@users.noreply.github.com>
2026-06-07 18:36:11 -0600

    Refine section watermarks

--- data/site-content.js
+++ data/site-content.js
@@ -1,4 +1,4 @@
-import { bigBangLegacyContent } from "./legacy-big-bang.js?v=20260607-side-rail-v1";
+import { bigBangLegacyContent } from "./legacy-big-bang.js?v=20260607-mature-watermarks-v1";
 
 function htmlSource(group, id) {
   return {
