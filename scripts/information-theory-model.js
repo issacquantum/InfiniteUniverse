d548c4c53bb0fb2ab681dbe1abaa38e9b247effd
Issac Tabares <issacquantum@users.noreply.github.com>
2026-06-08 18:35:19 -0600

    Refine watermark photon effect

--- scripts/information-theory-model.js
+++ scripts/information-theory-model.js
@@ -1,4 +1,4 @@
-import { bindPinchZoom } from "./model-pan.js?v=20260608-spanish-bound-cleanup-v1";
+import { bindPinchZoom } from "./model-pan.js?v=20260608-watermark-photon-v1";
 
 const mountedModels = new WeakSet();
 const SYMBOL_COUNT = 4;
