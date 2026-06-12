80bdf81ed0aaa1de2f64d2204ce83c3b6bcc9f14
Issac Tabares <issacquantum@users.noreply.github.com>
2026-06-11 21:25:34 -0600

    Fix gallery lightbox alt text

--- scripts/foundation-models.js
+++ scripts/foundation-models.js
@@ -1,4 +1,4 @@
-import { bindPinchZoom } from "./model-pan.js?v=20260608-watermark-photon-v1";
+import { bindPinchZoom } from "./model-pan.js?v=20260611-gallery-alt-v1";
 
 const mountedModels = new WeakSet();
 let threePromise = null;
