eb2a6100b43fb73089ca2a36f347c11c80c50eb2
Issac Tabares <issacquantum@users.noreply.github.com>
2026-06-14 10:02:50 -0600

    Trim vehicle wording from practice worlds

--- scripts/foundation-models.js
+++ scripts/foundation-models.js
@@ -1,4 +1,4 @@
-import { bindPinchZoom } from "./model-pan.js?v=20260614-systems-work-closing-trim-v1";
+import { bindPinchZoom } from "./model-pan.js?v=20260614-practice-worlds-vehicle-trim-v1";
 
 const mountedModels = new WeakSet();
 let threePromise = null;
