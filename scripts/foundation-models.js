29fa6c30eaa43f18bb4ad3a3df8f0908ffd36ff6
Issac Tabares <issacquantum@users.noreply.github.com>
2026-06-14 13:22:38 -0600

    Use desktop content font for titles

--- scripts/foundation-models.js
+++ scripts/foundation-models.js
@@ -1,4 +1,4 @@
-import { bindPinchZoom } from "./model-pan.js?v=20260614-photon-tabs-equations-icons-v1";
+import { bindPinchZoom } from "./model-pan.js?v=20260614-desktop-title-font-v1";
 
 const mountedModels = new WeakSet();
 let threePromise = null;
--- scripts/reading-settings.js
+++ scripts/reading-settings.js
@@ -1,4 +1,4 @@
-import { pick } from "./i18n.js?v=20260614-photon-tabs-equations-icons-v1";
+import { pick } from "./i18n.js?v=20260614-desktop-title-font-v1";
 
 const STORAGE_KEY = "issac-tabares-reading-settings";
 
