343d6a94e521b80f29425daf333e82d0d91a967a
Issac Tabares <issacquantum@users.noreply.github.com>
2026-06-14 09:06:54 -0600

    Refine learning path wording

--- scripts/information-theory-model.js
+++ scripts/information-theory-model.js
@@ -1,4 +1,4 @@
-import { bindPinchZoom } from "./model-pan.js?v=20260614-origins-self-focus-v1";
+import { bindPinchZoom } from "./model-pan.js?v=20260614-learning-path-redaction-v1";
 
 const mountedModels = new WeakSet();
 const SYMBOL_COUNT = 4;
