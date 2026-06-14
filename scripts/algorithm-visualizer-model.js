c60a2e437396ad08e8e1f2863677574a763822bb
Issac Tabares <issacquantum@users.noreply.github.com>
2026-06-14 13:52:20 -0600

    Keep science menu tabs stable

--- scripts/algorithm-visualizer-model.js
+++ scripts/algorithm-visualizer-model.js
@@ -1,4 +1,4 @@
-import { bindPinchZoom, isModelPanGesture, panTargetFromPointer } from "./model-pan.js?v=20260614-reset-expanded-tabs-v1";
+import { bindPinchZoom, isModelPanGesture, panTargetFromPointer } from "./model-pan.js?v=20260614-science-menu-layout-v1";
 
 const mountedModels = new WeakSet();
 let threePromise = null;
