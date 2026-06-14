9eb7238e58ff2ddd6085f849f8b3d2c6407aa380
Issac Tabares <issacquantum@users.noreply.github.com>
2026-06-14 09:44:19 -0600

    Correct guitar origin story

--- data/site-content.js
+++ data/site-content.js
@@ -1,4 +1,4 @@
-import { bigBangLegacyContent } from "./legacy-big-bang.js?v=20260614-music-influences-v1";
+import { bigBangLegacyContent } from "./legacy-big-bang.js?v=20260614-guitar-origin-v1";
 
 function htmlSource(group, id) {
   return {
