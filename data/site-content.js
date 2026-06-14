e5f30230e3985ba16c80f007331cd6382aa2a12b
Issac Tabares <issacquantum@users.noreply.github.com>
2026-06-14 14:05:43 -0600

    Add static infinity photon glow

--- data/site-content.js
+++ data/site-content.js
@@ -1,4 +1,4 @@
-import { bigBangLegacyContent } from "./legacy-big-bang.js?v=20260614-science-menu-layout-v1";
+import { bigBangLegacyContent } from "./legacy-big-bang.js?v=20260614-infinity-photon-v1";
 
 function htmlSource(group, id) {
   return {
