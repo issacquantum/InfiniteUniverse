c2cc71ae6eb454b187c3d3eba9b99faba2b8ba1e
Issac Tabares <issacquantum@users.noreply.github.com>
2026-06-14 09:33:16 -0600

    Move computing origin into learning path

--- scripts/information-theory-model.js
+++ scripts/information-theory-model.js
@@ -1,4 +1,4 @@
-import { bindPinchZoom } from "./model-pan.js?v=20260614-personal-process-trim-v1";
+import { bindPinchZoom } from "./model-pan.js?v=20260614-computing-origin-v1";
 
 const mountedModels = new WeakSet();
 const SYMBOL_COUNT = 4;
