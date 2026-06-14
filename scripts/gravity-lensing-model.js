1f48a09809bfaf27d9e62f03f4e1217cc05817e3
Issac Tabares <issacquantum@users.noreply.github.com>
2026-06-14 14:05:43 -0600

    Add static infinity photon glow

--- scripts/gravity-lensing-model.js
+++ scripts/gravity-lensing-model.js
@@ -1,4 +1,4 @@
-import { bindPinchZoom } from "./model-pan.js?v=20260614-science-menu-layout-v1";
+import { bindPinchZoom } from "./model-pan.js?v=20260614-infinity-photon-v1";
 
 const mountedModels = new WeakSet();
 const TWO_PI = Math.PI * 2;
