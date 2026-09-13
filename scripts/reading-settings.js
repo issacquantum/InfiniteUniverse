c3c52505f63b1268789f20e63a8608baf309ea71
Issac Tabares <issacquantum@users.noreply.github.com>
2026-09-13 13:17:36 -0600

    Social links

--- scripts/reading-settings.js
+++ scripts/reading-settings.js
@@ -1,4 +1,4 @@
-import { pick } from "./i18n.js?v=20260620-covered-tabs-only-v1";
+import { pick } from "./i18n.js?v=20260913-social-dock-v1";
 
 const STORAGE_KEY = "issac-tabares-reading-settings";
 
