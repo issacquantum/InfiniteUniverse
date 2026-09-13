3cbe0481b6fc16cbaf52f25383be1026d0aa1fbc
Issac Tabares <issacquantum@users.noreply.github.com>
2026-09-13 13:17:36 -0600

    Remove Facebook

--- data/site-content.js
+++ data/site-content.js
@@ -1,4 +1,4 @@
-import { bigBangLegacyContent } from "./legacy-big-bang.js?v=20260620-covered-tabs-only-v1";
+import { bigBangLegacyContent } from "./legacy-big-bang.js?v=20260913-social-dock-v1";
 
 function htmlSource(group, id) {
   return {
